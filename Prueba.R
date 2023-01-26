ee110269f45217101c4b35e6cf1ad03504cec048
FeliRad-sum <80574842+FeliRad-sum@users.noreply.github.com>
2023-01-26 18:44:37 +0100

    Depuración

--- Prueba.R
+++ Prueba.R
@@ -1,4 +1,14 @@
 #Carga datos
 mortalidad<-read.csv("Input/mortalidad.csv",sep=";")
 renta<-read.csv("Input/renta.csv",sep=";")
-
+# Depuración
+colnames(renta)[1]<-"provincia"
+colnames(renta)[2]<-"indicadores"
+renta<-subset(renta,indicadores=="Renta neta media por persona ")
+renta_filt<-renta[,-2]
+colnames(mortalidad)[1]<-"causa"
+mortalidad<-subset(mortalidad,Periodo>2014)
+mortalidad<-subset(mortalidad,Periodo<2021)
+colnames(mortalidad)[3]<-"provincia"
+colnames(mortalidad)[5]<-"num_muertes"
+colnames(renta_filt)[3]<-"renta"
